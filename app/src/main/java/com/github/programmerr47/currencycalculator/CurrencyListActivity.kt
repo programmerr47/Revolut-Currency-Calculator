087df343cbc315d170a8b0679feff720343f1faf
Michael <spitsinmichael@gmail.com>
2018-10-06 18:53:26 +0300

    #1: Converted support libraries to androidx

--- app/src/main/java/com/github/programmerr47/currencycalculator/CurrencyListActivity.kt
+++ app/src/main/java/com/github/programmerr47/currencycalculator/CurrencyListActivity.kt
@@ -1,7 +1,7 @@
 package com.github.programmerr47.currencycalculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class CurrencyListActivity : AppCompatActivity() {
 
