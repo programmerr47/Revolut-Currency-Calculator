a1adc595234658cf470088aa3b7b71c7e934b1f7
Michael <spitsinmichael@gmail.com>
2018-10-06 11:53:11 +0300

    Initial Commit

---
+++ app/src/main/java/com/github/programmerr47/currencycalculator/CurrencyListActivity.kt
@@ -0,0 +1,12 @@
+package com.github.programmerr47.currencycalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrencyListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency_list)
+    }
+}
