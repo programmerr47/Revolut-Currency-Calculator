7ba71515488aa882ec40b77254193a036a14fecd
Michael <spitsinmichael@gmail.com>
2018-10-08 10:41:42 +0300

    #6: wrapped current logic in special calculator

--- app/src/main/java/com/github/programmerr47/currencycalculator/CurrencyListActivity.kt
+++ app/src/main/java/com/github/programmerr47/currencycalculator/CurrencyListActivity.kt
@@ -3,6 +3,11 @@ package com.github.programmerr47.currencycalculator
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event.ON_PAUSE
+import androidx.lifecycle.Lifecycle.Event.ON_RESUME
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
@@ -14,36 +19,45 @@ class CurrencyListActivity : AppCompatActivity() {
 
     private val serverApi: ServerApi by inject()
     private val adapter: CurrencyListAdapter by lazy { CurrencyListAdapter() }
-
-    private var timerDisposable: Disposable? = null
+    private val calculator: CurrencyCalculator by lazy { CurrencyCalculator(
+            serverApi,
+            { adapter.update(it) },
+            { Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show() }
+    ) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency_list)
+        lifecycle.addObserver(calculator)
 
         bind<RecyclerView>(R.id.rv_list) {
             adapter = this@CurrencyListActivity.adapter
         }
     }
+}
 
-    override fun onResume() {
-        super.onResume()
+class CurrencyCalculator(
+        private val serverApi: ServerApi,
+        private val listener: (List<CurrencyItem>) -> Unit,
+        private val errorListener: (Throwable) -> Unit
+) : LifecycleObserver {
+    private var timerDisposable: Disposable? = null
 
+    @OnLifecycleEvent(ON_RESUME)
+    fun startTracking() {
         timerDisposable = Observable.interval(1, SECONDS)
                 .flatMapSingle { serverApi.getLatest().map {
                     it.rates.entries.map { CurrencyItem(it.key, it.value) }
                 } }
                 .observeOn(mainThread())
                 .subscribe(
-                        { adapter.update(it) },
-                        { Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show() }
+                        { listener(it) },
+                        { errorListener(it) }
                 )
-
     }
 
-    override fun onPause() {
-        super.onPause()
-
+    @OnLifecycleEvent(ON_PAUSE)
+    fun stopTracking() {
         timerDisposable?.let {
             if (!it.isDisposed)
                 it.dispose()
